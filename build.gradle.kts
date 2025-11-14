ee723ab795f009d4cda9a07203607fb1d8605444
lumentae <66220444+lumentae@users.noreply.github.com>
2025-11-14 21:23:15 +0100

    Downgrade jackson-databind version in dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     api("org.jspecify:jspecify:1.0.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
